f28948dd7bcd2f41400be1aebaf1f4daf18e181f
StephenGregan <grego@live.ie>
2018-06-19 17:19:10 +0100

    initital commit

---
+++ AzureSearchIndex/Account.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSearchIndex
+{
+    [SerializePropertyNamesAsCamelCase]
+    public class Account
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        [IsFilterable]
+        public string Account_Number { get; set; }
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public double? Balance { get; set; }
+
+        [IsFilterable, IsSortable, IsSearchable]
+        public string FirstName { get; set; }
+
+        [IsFilterable, IsSortable, IsSearchable]
+        public string LastName { get; set; }
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public int? Age { get; set; }
+
+        [IsFilterable, IsSortable, IsSearchable]
+        public string Gender{ get; set; }
+
+        [IsFilterable, IsSearchable]
+        public string Address{ get; set; }
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public string Employer{ get; set; }
+
+        [IsFilterable]
+        public string Email{ get; set; }
+
+        [IsFilterable, IsSortable, IsSearchable]
+        public string City{ get; set; }
+
+        [IsFilterable, IsSortable, IsSearchable]
+        public string State{ get; set; }
+    }
+}
---
+++ AzureSearchIndex/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+namespace AzureSearchIndex
+{
+    class Program
+    {
+        static string searchServiceName = ConfigurationManager.AppSettings["StephenSearchName"];
+        static string searchServiceAdminKey = ConfigurationManager.AppSettings["StephenSearchServiceAdminKey"];
+        static string searchServiceQueryKey = ConfigurationManager.AppSettings["StephenSearchServiceQueryKey"];
+
+        static void Main(string[] args)
+        {
+            SearchServiceClient serviceClient = new SearchServiceClient
+                (searchServiceName, new SearchCredentials(searchServiceAdminKey));
+
+            if (serviceClient.Indexes.Exists("accounts"))
+            {
+                serviceClient.Indexes.Delete("accounts");
+            }
+
+            var accountIndexDefinition = new Index()
+            {
+                Name = "accounts",
+                Fields = FieldBuilder.BuildForType<Account>()
+            };
+
+            serviceClient.Indexes.Create(accountIndexDefinition);
+
+            ISearchIndexClient indexClient = serviceClient.Indexes.GetClient("accounts");
+
+            ImportDocuments(indexClient);
+        }
+
+        private static void ImportDocuments(ISearchIndexClient indexClient)
+        {
+            var actions = new List<IndexAction<Account>>();
+
+            string line;
+
+            using (System.IO.StreamReader file = new System.IO.StreamReader("accounts.json"))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    JObject json = JObject.Parse(line);
+                    Account account = json.ToObject<Account>();
+                    actions.Add(IndexAction.Upload(account));
+                }
+                file.Close();
+            }
+
+            var batch = IndexBatch.New(actions);
+
+            try
+            {
+                indexClient.Documents.Index(batch);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
